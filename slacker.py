69a44d6e3177cc98fdb6de51b529c78c0d70b3ba
Javier Lopez <elcupto@gmail.com>
2020-02-12 01:05:14 -0800

    Update slacker.py

--- slacker.py
+++ slacker.py
@@ -89,6 +89,14 @@ class Slacker(WithLogger, WithConfig):
                 murl += "&latest={}".format(int(time.time()))
             payload = self.get_with_retry_to_json(murl)
             self.logger.debug("test: %s", payload)
+            if payload['ok'] is False:
+            	post_data_join = {
+        			'token': self.token,
+            		'channel': cid
+        		}
+            	self.logger.debug("Join in %s, in %s", self.url + "conversations.join", post_data_join)
+        		self.session.post(self.url + "conversations.join", data=post_data_join)
+        		payload = self.get_with_retry_to_json(murl)
             messages += payload['messages']
             if payload['has_more'] is False:
                 done = True
