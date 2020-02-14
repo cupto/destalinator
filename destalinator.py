fdfbe4e851c8537e160e5bcd3aea6e617266dee6
Javier Lopez <elcupto@gmail.com>
2020-02-14 14:35:53 -0800

    Update destalinator.py

--- destalinator.py
+++ destalinator.py
@@ -124,21 +124,23 @@ class Destalinator(WithLogger, WithConfig):
         messages = self.get_messages(channel_name, days)
         self.logger.debug("Stale: Ignored Users: %s", ''.join(self.config.ignore_users))
         for xm in messages:
-            self.logger.debug("Stale: User: %s, Username: %s, BotName: %s", xm.get("user"), xm.get("username"), xm.get("bot_profile").get("name"))
+            xmbot = xm.get("bot_profile").get("name") if xm.get("bot_profile") else None 
+            self.logger.debug("Stale: User: %s, Username: %s, BotName: %s", xm.get("user"), xm.get("username"), xmbot)
             if xm.get("user") in self.config.ignore_users:
                 self.logger.debug("Stale: User %s ignored in %s", xm.get("user"),channel_name)
             if xm.get("name") in self.config.ignore_users:
                 self.logger.debug("Stale: Username %s ignored in %s", xm.get("username"),channel_name)
             if xm.get("bot_profile").get("username") in self.config.ignore_users:
-                self.logger.debug("Stale: Username %s ignored in %s", xm.get("bot_profile").get("name"),channel_name)
+                self.logger.debug("Stale: Username %s ignored in %s", xmbot,channel_name)
                 
 
         # return True (stale) if none of the messages match the criteria below
         return not any(
+            xbot = x.get("bot_profile").get("name") if x.get("bot_profile") else None  
             # the message is not from an ignored user
             x.get("user") not in self.config.ignore_users \
             and x.get("username") not in self.config.ignore_users \
-            and x.get("bot_profile").get("username") not in self.config.ignore_users \
+            and xbot not in self.config.ignore_users \
             and (
                 # the message must have text that doesn't include ignored words
                 (x.get("text") and b":dolphin:" not in x.get("text").encode('utf-8', 'ignore')) \
